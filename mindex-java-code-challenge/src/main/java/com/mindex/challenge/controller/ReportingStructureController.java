2e5a25a61c8f18fa1273d20019619ce7c672872d
jdavis592 <davis.jordan592@gmail.com>
2024-03-30 00:13:24 -0500

    ReportingStructureController implementation for calculate endpoint

---
+++ mindex-java-code-challenge/src/main/java/com/mindex/challenge/controller/ReportingStructureController.java
@@ -0,0 +1,25 @@
+package com.mindex.challenge.controller;
+
+import com.mindex.challenge.service.ReportingStructureService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ReportingStructureController {
+    private static final Logger LOG = LoggerFactory.getLogger(ReportingStructureController.class);
+
+    @Autowired
+    private ReportingStructureService reportingStructureService;
+
+    @GetMapping("/reportingStructure/{id}")
+    public int CalculateNumberOfReports(@PathVariable String id) {
+        LOG.debug("Received CalculateNumberOfReports request for id[{}]", id);
+
+        return reportingStructureService.CalculateNumberOfReports(id);
+    }
+}
