03b89dce19c2b1a0f29152e9776dea0cc2961a9b
jdavis592 <davis.jordan592@gmail.com>
2024-03-30 00:14:00 -0500

    Starting test  case for the calcualtion endpoint

---
+++ mindex-java-code-challenge/src/test/java/com/mindex/challenge/service/impl/ReportingStructureServiceImplTest.java
@@ -0,0 +1,51 @@
+package com.mindex.challenge.service.impl;
+
+import com.mindex.challenge.data.Employee;
+import com.mindex.challenge.data.ReportingStructure;
+import com.mindex.challenge.service.ReportingStructureService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ReportingStructureServiceImplTest {
+
+    private String reportingStructureUrl;
+
+    @Autowired
+    private ReportingStructureService reportingStructureService;
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Before
+    public void setup() {
+        reportingStructureUrl = "http://localhost:" + port + "/reportingStructure/{id}";
+    }
+
+    @Test
+    public void testCalculateNumberOfReports() {
+        String employeeId = "16a596ae-edd3-4847-99fe-c4518e82c86f";
+        int expectedReports = 4;
+
+        int calculatedReports = reportingStructureService.CalculateNumberOfReports(employeeId);
+
+        assertEquals(expectedReports, calculatedReports);
+    }
+}
